20b200062ec7e1b245a485065a64e41bf1f5c874
jecode93 <jeanemmanuelcadet@gmail.com>
2023-10-26 15:51:31 -0400

    Add validations for User and Post models

--- app/models/post.rb
+++ app/models/post.rb
@@ -8,9 +8,23 @@ class Post < ApplicationRecord
     Comment.where(post_id: id).order(created_at: :desc).limit(5)
   end
 
-  private
+  private :update_post_counter
 
   def update_post_counter
     author.increment!(:posts_counter)
   end
+  
+  # Add validations
+  
+  # Title must not be blank.
+  validates :title, presence: true
+  
+  # Title must not exceed 250 characters.
+  validates :title, length: { maximum: 250 }
+  
+  # CommentsCounter must be an integer greater than or equal to zero.
+  validates :comments_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  
+  # LikesCounter must be an integer greater than or equal to zero.
+  validates :likes_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 end
