0519b467a3abe2e890b3e3362d328cf4d1bf3db9
jecode93 <jeanemmanuelcadet@gmail.com>
2023-10-30 13:31:07 -0400

    Create routes only for the 4 required URLs

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  get '/users', to: "users#index"
+  get '/users/:id', to: "users#show"
+  get '/users/:user_id/posts', to: "posts#index"
+  get '/users/:user_id/posts/:id', to: "users#show"
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
