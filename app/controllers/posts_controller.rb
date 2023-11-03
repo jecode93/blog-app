ea2f84c2675e55e8932fdf83956cf9397a61800e
jecode93 <jeanemmanuelcadet@gmail.com>
2023-11-03 00:04:12 -0400

    Complete add-forms milestone

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,4 +6,20 @@ class PostsController < ApplicationController
   def show
     @post = Post.find(params[:id])
   end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(params.require(:post).permit(:title, :text))
+    @post.author = current_user
+    if @post.save
+      flash[:success] = 'Post created successfully!'
+      redirect_to user_posts_url
+    else
+      flash.now[:error] = 'Error: Post could not be created!'
+      render :new, locals: { post: @post }
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -13,7 +13,10 @@ Rails.application.routes.draw do
   #CODE REVIEWER optional comment
   # [OPTIONAL] While this is going to work fine, I will encourage you to take advantage of resources by having something like:
   resources :users, only: [:index, :show] do
-    resources :posts, only: [:index, :show]
+    resources :posts, only: [:index, :new, :create, :show] do
+      resources :comments, only: [:new, :create]
+      resources :likes, only: [:create]
+    end
   end
   # With this, you have shorter code to write and you keep it simple, stupid.
   
